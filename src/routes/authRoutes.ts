97126d4a2f2f1749d15eb2e4d9980ae97049927b
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2024-09-01 19:30:00 +0500

    Sign in API completed

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,4 +5,6 @@ const authController = require("../controllers/authController");
 
 router.post("/auth/signup", authController.signup);
 
+router.post("/auth/signin", authController.signin);
+
 module.exports = router;
