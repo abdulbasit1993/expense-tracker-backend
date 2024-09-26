a540e50b2c04a14bc9a230c882a49b3b2c004eed
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2024-09-26 21:29:16 +0500

    prepare project for deployment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
@@ -10,7 +10,7 @@ import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
 
-app.get("/", async (req, res) => {
+app.get("/api", async (req, res) => {
   res.send({ message: "Welcome to Expense Tracker APIs!" });
 });
 
