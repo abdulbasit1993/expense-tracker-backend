17b2612df04c2f8a85bf130d8cca0b3ab9765870
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2024-11-03 10:42:24 +0500

    created APIs for get all Expense of User, Update Expense, and Delete Expense

--- src/routes/expenseRoutes.ts
+++ src/routes/expenseRoutes.ts
@@ -5,4 +5,10 @@ const router = express.Router();
 
 router.post("/expenses/add", extractUser, expenseController.addNewExpense);
 
+router.get("/expenses", extractUser, expenseController.getAllExpensesOfUser);
+
+router.put("/expenses/update", extractUser, expenseController.updateExpense);
+
+router.delete("/expenses/delete", extractUser, expenseController.deleteExpense);
+
 export default router;
